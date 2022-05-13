cc8df3e5c843bea29192252d89aabb7b951a3617
enmanuelmag <enmanuelmag@cardor.dev>
2022-05-12 19:48:59 -0500

    Fix code error

--- .github/scripts/checker.version.py
+++ .github/scripts/checker.version.py
@@ -8,19 +8,19 @@ command = 'git show origin/master:documentation/versions.json'
 output = subprocess.check_output(command, shell=True)
 version_master = output.decode('utf-8')
 version_master = json.loads(version_master)
-
-print("Version master:", version_master)
 last_version_master = int(version_master[-1].replace('.', ''))
 
 path_version_local = os.path.join(os.getcwd(), 'documentation', 'versions.json')
 with open(path_version_local, 'r') as f:
   version_local = json.load(f)
-  print("Version local:", version_local)
   last_version_local = int(version_local[-1].replace('.', ''))
+
+print("Last version master:", last_version_master)
+print("Last version local:", last_version_local)
 
 if (last_version_local > last_version_master):
   print("Local version is greater than master version")
-  exit(1)
+  exit(0)
 else:
   print("Local version is equal or less than master version")
-  exit(0)
+  exit(1)
