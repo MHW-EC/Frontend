7a80f2b9bf238b5cdd6289fe60fb8265837e2829
enmanuelmag <enmanuelmag@cardor.dev>
2023-12-31 21:20:29 -0500

    Finish styles fixes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,57 +1,39 @@
-
 import React from 'react';
-import {
-  Button,
-  Paper,
-  Grid
-} from '@mui/material';
-import {
-  Link
-} from 'react-router-dom';
+import { Button, Paper, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <Paper
       square
       sx={{
-        minHeight: '100vh',
-        maxHeight: '100%',
         display: 'flex',
         margin: 'auto',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
-      }}>
+        alignItems: 'center',
+      }}
+    >
       <Grid
         container
         direction="column"
         alignItems="center"
         justifyContent="center"
-        >
-          <Grid item>
-            <img src='/logo192.png' alt='' />
-          </Grid>
+      >
+        <Grid item>
+          <img src="/logo192.png" alt="" />
         </Grid>
+      </Grid>
       <Link
         style={{ textDecoration: 'none', width: '250px' }}
-        to={'/simple-schedule'}>
-        <Button
-          sx={{ mt: 4 }}
-          fullWidth
-          variant="contained"
-          color="primary">
+        to={'/simple-schedule'}
+      >
+        <Button sx={{ mt: 4 }} fullWidth variant="contained" color="primary">
           {'Simple schedule'}
         </Button>
       </Link>
-      <Link
-        style={{ textDecoration: 'none', width: '250px' }}
-        to={'/'}>
-        <Button
-          sx={{ mt: 2 }}
-          variant="contained"
-          color="primary"
-          fullWidth
-        >
+      <Link style={{ textDecoration: 'none', width: '250px' }} to={'/'}>
+        <Button sx={{ mt: 2 }} variant="contained" color="primary" fullWidth>
           {'Multiplayer mode (soon)'}
         </Button>
       </Link>
